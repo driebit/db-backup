57d33affab54d56dadd9c0f2636349d27dfc89f7
David de Boer <david@ddeboer.nl>
2015-12-21 15:57:46 +0100

    Add MongoDB host param

--- src/MongoDumpBackup.php
+++ src/MongoDumpBackup.php
@@ -6,10 +6,17 @@ class MongoDumpBackup extends AbstractCliBackup
 {
     protected $dumpBinary = 'mongodump';
     protected $restoreBinary = 'mongorestore';
-    
+    protected $host;
+
+    public function __construct($host = null)
+    {
+        $this->host = $host;
+    }
+
     public function backup($database, $file, array $params = array())
     {
         $arguments = array(
+            '--host', $this->host,
             '-d', $database,
             '-o', $file
         );
